9ff54c615c0ca6a5b4a9dca923ec3094b1f69d5f
shashiki <shashiki@pdx.edu>
2023-11-10 15:06:33 -0800

    parsing file update

--- parsing.sv
+++ parsing.sv
@@ -1,10 +1,10 @@
-/Parsing file to read and write input trace file 
+//Parsing file to read and write input trace file 
 
 module parsing_tb#(parameter ENABLE_TEST=0);
 int file_read;   // to read a input trace file
 int file_write;  // to write into output trace file
 int read_value;
-longint  time_unit;
+int  time_unit;
 logic [11:0] core;
 logic [1:0] operation;
 logic [35:0] address;
@@ -16,26 +16,25 @@ task parsing();
   if(ENABLE_TEST) begin    //switch to ENABLE/DISABLE 
     if($value$plusargs("input_file=%s",line))begin
       //Read a input trace file through command line
-      file_read =$fopen(line,"r+");
+      file_read =$fopen(line,"r");
       if(file_read) begin
         $display("trace file read =%0d",file_read);
       end
       // Writes data to output.txt file
-      file_write=$fopen("output.txt", "w");
+      file_write=$fopen("dram.txt", "w");
       if(file_write) begin
-        longint i;
-        while(1) begin
-          read_value= $fscanf(file_read, "%d %d %d %h", time_unit, core, operation, address);
-          if($fgets(line,file_read)) begin
-            $display("the value of time=%d core=%12d operation=%h, address=%h ",  time_unit, core, operation, address); 
-            $fwrite(file_write, "time value=%d \t core=%d \t operation=%2h \t address=%h \n", time_unit,core,operation,address);
-            i++;
-          end
-          else begin
-            break;    // exit after the last line
-          end
-
-        end
+        int i;
+       while (1) begin
+  read_value = $fscanf(file_read, "%d %d %d %h", time_unit, core, operation, address);
+  if (read_value == 4) begin
+    $display("the value of time=%d, core=%12d, operation=%h, address=%h ", time_unit, core, operation, address);
+    $fwrite(file_write, "time value=%d core=%d  operation=%2h  address=%h \n", time_unit, core, operation, address);
+    i++;
+  end
+  else begin
+    break; // exit after the last line
+  end
+end
                         
       end
       $fclose(file_read);      
@@ -54,4 +53,3 @@ initial begin
   $finish;
 end
 endmodule
-
